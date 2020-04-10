4e101f5593380b3bbde1d0eb57572901957d3557
balexnick <leha.alexey1909@gmail.com>
2020-04-10 18:09:53 +0300

    Updates

---
+++ precache-manifest.573e7e3c617b69da96583ba97c863704.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "debe626ae9e9934900eb4a980bbbc8c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13e9aa811bd66b48675c",
+    "url": "/static/css/2.8495ce49.chunk.css"
+  },
+  {
+    "revision": "5b418662484f31b10b56",
+    "url": "/static/css/main.41b0cd91.chunk.css"
+  },
+  {
+    "revision": "13e9aa811bd66b48675c",
+    "url": "/static/js/2.855f43b8.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.855f43b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b418662484f31b10b56",
+    "url": "/static/js/main.95f006fb.chunk.js"
+  },
+  {
+    "revision": "7155600a52b993b5ffda",
+    "url": "/static/js/runtime-main.1cb82820.js"
+  },
+  {
+    "revision": "0f5e712aa515b0792b7527a18b6ca647",
+    "url": "/static/media/loader.0f5e712a.gif"
+  }
+]);
